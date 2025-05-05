db7b48a16dd289cce4b5f04b482116745d03ba1d
Pravallika583 <pravallika.peddi12@gmail.com>
2025-05-05 20:52:04 +0000

    Implement user authentication with JWT and protected route handling

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,8 +1,51 @@
 import './index.css'
 import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
 
 class Login extends Component {
+  state = {username: '', password: '', errorMsg: '', showSubmitError: false}
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  onSubmitSuccess = jwtToken => {
+    const {history} = this.props
+    Cookies.set('jwt_token', jwtToken, {expires: 30})
+    history.replace('/')
+  }
+
+  onSubmitFailure = msg => {
+    this.setState({errorMsg: msg, showSubmitError: true})
+  }
+
+  onLogin = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const userDetails = {username, password}
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch('https://apis.ccbp.in/login', options)
+    const data = await response.json()
+    if (response.ok === true) {
+      this.onSubmitSuccess(data.jwt_token)
+    } else {
+      this.onSubmitFailure(data.error_msg)
+    }
+  }
+
   render() {
+    const {username, password, errorMsg, showSubmitError} = this.state
+    if (Cookies.get('jwt_token') !== undefined) {
+      return <Redirect to="/" />
+    }
     return (
       <div className="bg-container">
         <div className="login-container">
@@ -11,24 +54,39 @@ class Login extends Component {
             alt="website logo"
             className="website-logo"
           />
-          <form className="form-container">
+          <form className="form-container" onSubmit={this.onLogin}>
             <div className="input-field">
               <label className="label" htmlFor="username">
                 USERNAME
               </label>
               <br />
-              <input id="username" placeholder="Username" className="input" />
+              <input
+                id="username"
+                type="text"
+                placeholder="Username"
+                className="input"
+                onChange={this.onChangeUsername}
+                value={username}
+              />
             </div>
             <div className="input-field">
-              <label className="label" htmlFor="username">
+              <label className="label" htmlFor="password">
                 PASSWORD
               </label>
               <br />
-              <input id="username" placeholder="Password" className="input" />
+              <input
+                id="password"
+                type="password"
+                placeholder="Password"
+                className="input"
+                onChange={this.onChangePassword}
+                value={password}
+              />
             </div>
             <button type="submit" className="login-button">
               Login
             </button>
+            {showSubmitError && <p className="error-msg">*{errorMsg}</p>}
           </form>
         </div>
       </div>
